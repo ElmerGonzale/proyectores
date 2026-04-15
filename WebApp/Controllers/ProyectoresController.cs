4f07b9ce52366ff4fac3a8299306495b3800b77e
Elmer Bandian Gonzalez Haas <23470134@campeche.tecnm.mx>
2026-04-15 08:48:02 -0600

    Primera versión del programa

---
+++ WebApp/Controllers/ProyectoresController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Xml.Linq;
+
+namespace WebApp.Controllers
+{
+    public class ProyectoresController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Content(
+                "<p>Aqu&iacute se mostrar&aacute la lista de proyectores registrados</p>",
+                "text/html"
+                );
+        }
+        public IActionResult Details(int id)
+        {
+            string cadena = "<h1> Nuevo proyector </h1>";
+            cadena += $"<p> Aqu&iacute se mostrar&aacute la informaci&oacuten del proyector espec&iacutefico: {id} </p>";
+            return Content(cadena, "text/html");
+        }
+
+        public IActionResult Delete(int id)
+        {
+            string cadena = $"<h1> Dar de baja </h1>" +
+                $"<p> Aqui se dara de baja el proyector: {id} </p>";
+            return Content(cadena, "text/html");
+        }
+
+        public IActionResult Edit(int id)
+        {
+            string cadena = $"<h1> Editar proyector </h1>" +
+                $"<p> Aqui se modificara el proyector: {id} </p>";
+            return Content(cadena, "text/html");
+        }
+
+        
+    }
+}
