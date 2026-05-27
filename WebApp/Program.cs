24aeb05e66eb00c2bfbd4da123e16944611a2ceb
Elmer Bandian Gonzalez Haas <23470134@campeche.tecnm.mx>
2026-05-27 09:45:08 -0600

    Agrega proyectores a la base de datos

--- WebApp/Program.cs
+++ WebApp/Program.cs
@@ -1,7 +1,24 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using WebApp.Data;
+using WebApp.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+//Agregar contexto de la BD como un servicio
+builder.Services.AddDbContext<AppDbContext>(
+    options => 
+    //options.usesqlserver(builder.configuration.getconnectionstring("conexion"))
+    options.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Proyectores;Integrated Security=True;" +
+                         "Connect Timeout=30;Encrypt=True;Trust Server Certificate=False;Application Intent=ReadWrite;" +
+                         "Multi Subnet Failover=False;Command Timeout=30")
+
+    );
+
+builder.Services.AddTransient<IProyectoresService, ProyectoresService>();
 
 var app = builder.Build();
 
